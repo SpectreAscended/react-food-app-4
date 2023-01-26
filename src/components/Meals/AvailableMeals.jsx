fad7af483f6b8f07a3d6d90887d091988394644d
Cory Pollard <astandard87@gmail.com>
2023-01-26 16:09:32 -0600

    Added fetch function to get meal data from firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItems/MealItem';
 import Card from '../UI/Card';
-import DUMMY_MEALS from '../../store/dummyMeals';
 
 const AvailableMeals = () => {
-  const meals = DUMMY_MEALS.map(meal => (
+  const [mealsData, setMealsData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const res = await fetch(
+        'https://react-http-41004-default-rtdb.firebaseio.com/meals.json'
+      );
+      if (!res.ok) throw new Error(`Something went wrong (${res.status})`);
+
+      const data = await res.json();
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setMealsData(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch(err => {
+      setIsLoading(false);
+      setHttpError(err.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes['meals-loading']}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes['meals-error']}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const meals = mealsData.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
